4627c6cb9940cab91cac92aa640cfb75fd18cfe8
Jihed ALOUI <gihedaloui@gmail.com>
2025-04-29 17:50:53 +0200

    add a database with exercises and memory state

--- app.py
+++ app.py
@@ -1,38 +1,25 @@
 #pylint: disable=missing-module-docstring
-import io
 import duckdb
-import pandas as pd
 import streamlit as st
 
 st.write("""SQL SRS Spaced Repetition SQL Practice""")
 
+#la connection a notre BD
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ("JOINS", "GROUP BY", "WINDOWS FUNCTIONS"),
+        ("cross_joins", "GROUP BY", "window_functions"),
+        index=None,
+        placeholder="Select one of the themes",
     )
 
-    st.write("You selected:", option)
-# DATA
-CSV = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
+    st.write("You selected:", theme)
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme ='{theme}'").df()
+    st.write(exercise)
 
-beverages = pd.read_csv(io.StringIO(CSV))
-
-CSV2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-
-food_items = pd.read_csv(io.StringIO(CSV2))
-
-# la r√©ponse que la personne doit taper
+# la reponse que la personne doit taper
 ANSWER_STR = """
 SELECT *
 FROM beverages
@@ -49,27 +36,27 @@ query = st.text_area(label="votre code sql ici", key="user input")
 
 # RESULTAT DE LA QUERY TAPEE PAR LA PERSONNE
 # st.write(duckdb.sql(query).df())
-
-if query:
-    result = duckdb.query(query).df()
-    st.dataframe(result)
-
-    try:
-        result = result[solution_tab.columns]
-    except KeyError as e:
-        st.write("some columns are missing")
-    st.dataframe(result.compare(solution_tab))
-
-
-tab1, tab2 = st.tabs(["Tables", "Solution"])
-
-with tab1:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("table: Expected")
-    st.dataframe(solution_tab)
-
-with tab2:
-    st.write(ANSWER_STR)
+#
+# if query:
+#     result = duckdb.query(query).df()
+#     st.dataframe(result)
+#
+#     try:
+#         result = result[solution_tab.columns]
+#     except KeyError as e:
+#         st.write("some columns are missing")
+#     st.dataframe(result.compare(solution_tab))
+#
+#
+# tab1, tab2 = st.tabs(["Tables", "Solution"])
+#
+# with tab1:
+#     st.write("table: beverages")
+#     st.dataframe(beverages)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("table: Expected")
+#     st.dataframe(solution_tab)
+#
+# with tab2:
+#     st.write(ANSWER_STR)
